c8ed53762d4a5df9c407691910fff8daebfe30c7
Your Name <debra4117@gmail.com>
2022-01-07 13:46:32 +0800

    update

--- src/binarytree/PostOrder.java
+++ src/binarytree/PostOrder.java
@@ -14,6 +14,36 @@ package binarytree;
  *     - Consequently, each node is placed on the stack 3 times during the course of the traversal.
  *     - If a node is popped from the stack a third time, we can mark it as the current node to be visited.
  *     
+ *     - Otherwise, the node is being popped for either the first time or the second time.
+ *     - In this case, it is not yet ready to be visited, so we push it back onto the stack
+ *       and simulate a recursive call.
+ *     - If the node was popped for a first time, we need to push the left child(if it exists) onto the stack.
+ *     - Otherwise, the node was popped for as second time, ams we  push the right child(if it exists) onto the stak.
+ *     - In any event, we then pop the stack, applying the same set. 
+ *     - Note that, when we pop the stack, we are simulating the recursive call to the appropriate child.
+ *     
+ *     - If the child does not exist and thus was never pushed onto the stack,
+ *       when we pop the stack we pop the original node again.
+ *       
+ *     - Eventually, either the process pops a node for the third time or the stack empties.
+ *     - In the latter case, we have iterated over the entire tree.
+ *     - We initialize the algorithm by pushing a reference to the root onto the stack.
+ *       
+ *       
+ *       
+ *     
+ *     
+ *   
+ *  - Let us trace through the postorder traversal.
+ *  - We initialize the traversal by pushing root a onto the stack.
+ *  - The first pop visits a.
+ *  - This is a's first pop, so it is placed back on the stack, and we push its left child, b, onto the stack.
+ *  
+ *  - Next b is popped.
+ *  - It is b's first pop, so its is placed back on the stack.
+ *  - Normally, b's left child would the be pushed, but b has no left child, so nothing is pushed.
+ *  - Thus the next pop reveals b for the second time, b is placed back on the stack,   
+ *     
  * 
  */
 public class PostOrder<AnyType> extends TreeIterator<AnyType> {
